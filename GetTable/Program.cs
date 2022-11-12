1cc2e8db891db66fbc9a84fdf8267e78259156df
negin gilani <zahra.attargilani@gmail.com>
2022-11-12 18:58:01 +0330

    first commit

---
+++ GetTable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlClient;
+
+Console.WriteLine("Enter server Name :...");
+string serverName = Console.ReadLine();
+
+Console.WriteLine("Enter Database Name :...");
+string databaseName = Console.ReadLine();
+
+Console.WriteLine("Enter User Name :...");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Enter Password :...");
+string password = Console.ReadLine();
+
+//Console.WriteLine($"connection string is : \n Server={serverName} ; Initial catalog= {databaseName} ; User Id={userName} ; Password={password}");
+
+
+var connString = @"Data Source=" + serverName + ";Initial Catalog="
+                     + databaseName + ";Persist Security Info=True;User ID=" + userName + ";Password=" + password;
+
+Console.WriteLine(connString);
+SqlConnection conn = new SqlConnection(connString);
+
+
+try
+{
+    Console.WriteLine("Openning Connection ...");
+
+    //open connection
+    conn.Open();
+
+    Console.WriteLine("Connection successful!");
+    SqlCommand cmd = new SqlCommand("select TABLE_NAME from  information_schema.tables  where TABLE_TYPE='BASE TABLE'", conn); 
+    using (SqlDataReader reader = cmd.ExecuteReader())
+{
+    while (reader.Read())
+    {
+        Console.WriteLine(reader[0]);
+    }
+}
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error: " + e.Message);
+}
+
+Console.Read();
+
